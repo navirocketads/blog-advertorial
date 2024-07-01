c515f1ced0873158467a55e5637da7b25c70da1b
Ivan mac air <ivanmacair@MacBook-Pro-de-Navi.local>
2024-07-01 13:31:42 +0200

    ai

--- APLICACOES/avaliador/server.js
+++ APLICACOES/avaliador/server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const { createProxyMiddleware } = require('http-proxy-middleware');
-// Define o diretório de arquivos estáticos
-app.use(express.static(path.join(__dirname, 'public')));
 
 
 // Configuração do proxy reverso para o quiz local (proxy black)
